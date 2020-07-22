2b618058e008c678eab734c10b04c1637de2abd7
henrique <hbaetaleite@gmail.com>
2020-07-22 13:05:25 -0300

    Updates

---
+++ precache-manifest.5dd4d5e118aea0619ff4b7017524a59b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd791375acb29be1c491c65ef8f762d6",
+    "url": "/HenriqueBaetaLeite/lista-de-tarefas.git/index.html"
+  },
+  {
+    "revision": "23cbe5a83129038d4bad",
+    "url": "/HenriqueBaetaLeite/lista-de-tarefas.git/static/css/main.f6c51823.chunk.css"
+  },
+  {
+    "revision": "19513375cea5f04dacd1",
+    "url": "/HenriqueBaetaLeite/lista-de-tarefas.git/static/js/2.02af7f22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HenriqueBaetaLeite/lista-de-tarefas.git/static/js/2.02af7f22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23cbe5a83129038d4bad",
+    "url": "/HenriqueBaetaLeite/lista-de-tarefas.git/static/js/main.7a43aee4.chunk.js"
+  },
+  {
+    "revision": "0e5b41a3bf1f1b086f3f",
+    "url": "/HenriqueBaetaLeite/lista-de-tarefas.git/static/js/runtime-main.77ed4ae7.js"
+  }
+]);
