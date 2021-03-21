8a52296f24ca1d05e1ca672adc837e7c76280157
henrique <hbaetaleite@gmail.com>
2021-03-21 09:50:12 -0300

    Alterando README e corrigindo link linkedin no footer

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
-const MyFooter = () => {
-  return (
-    <footer className="text-center">
-      <p>
-        Desenvolvido por{' '}
-        <a
-          className=""
-          href="https://www.linkedin.com/in/henrique-ba%C3%AAta-leite-785a4b15a/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Henrique Baêta Leite
-        </a>{' '}
-        - 2020{' '}
-        <a href="https://github.com/HenriqueBaetaLeite" rel="noopener noreferrer" target="_blank">
-          <i className="fa fa-github-alt" />
-        </a>
-      </p>
-    </footer>
-  );
-};
+const MyFooter = () => (
+  <footer className="text-center">
+    <p>
+      Desenvolvido por{' '}
+      <a
+        className=""
+        href="https://www.linkedin.com/in/henriquebaetaleite/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Henrique Baêta Leite
+      </a>{' '}
+      - 2020{' '}
+      <a href="https://github.com/HenriqueBaetaLeite" rel="noopener noreferrer" target="_blank">
+        <i className="fa fa-github-alt" />
+      </a>
+    </p>
+  </footer>
+);
 
 export default MyFooter;
